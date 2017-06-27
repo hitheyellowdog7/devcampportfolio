e989a38edf9c32c3a26dafd7b0076633604103fd
Chance McKean <hitheyellowdog7@gmail.com>
2017-06-27 17:49:00 +0000

    Integraetd technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
@@ -59,3 +61,10 @@ end
 
 puts "9 portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
