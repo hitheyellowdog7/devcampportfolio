273e3c1a77d990738d75ef3f35ad29d9399f3a12
Chance McKean <hitheyellowdog7@gmail.com>
2017-08-07 21:50:47 +0000

    Implemented action cable for comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
+  resources :topics, only: [:index, :show]
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -16,8 +17,8 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  
-  moutn ActionCable.server => '/cable'
+
+  mount ActionCable.server => '/cable'
   
   root to: 'pages#home'
 end
